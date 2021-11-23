fb7fc26b8b07ebeed78ca5e1f4b4fd393e1b3aa8
Evgeny Gerdt <assmys041298@yandex.ru>
2021-11-24 02:24:13 +0300

    Initial Commit

---
+++ party-joiner-mobile/party_joiner_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  party_joiner_mobileApp.swift
+//  party-joiner-mobile
+//
+//  Created by Evgeny Gerdt on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct party_joiner_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
